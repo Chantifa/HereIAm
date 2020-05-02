5c9409e57505d97f03b9f32f713334620a6c64fb
Oliver Bucher <level8broccoli@gmail.com>
2020-05-02 15:44:32 +0200

    show list with sample data

--- app/src/main/java/ch/ffhs/esa/hereiam/ui/fragments/EntryListFragment.kt
+++ app/src/main/java/ch/ffhs/esa/hereiam/ui/fragments/EntryListFragment.kt
@@ -1,6 +1,5 @@
 package ch.ffhs.esa.hereiam.ui.fragments
 
-import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -11,13 +10,13 @@ import android.widget.ListView
 import androidx.fragment.app.Fragment
 import ch.ffhs.esa.hereiam.R
 import ch.ffhs.esa.hereiam.model.Entry
-import com.google.firebase.Timestamp
 import com.google.firebase.firestore.FirebaseFirestore
 
 
 class EntryListFragment : Fragment() {
 
-    private var entries: ArrayList<Entry> = ArrayList<Entry>()
+    private var listView: ListView? = null
+    private var entries: ArrayList<String> = arrayListOf("Entry1", "Entry2", "Entry3")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,7 +24,7 @@ class EntryListFragment : Fragment() {
     ): View? {
 
         val view: View = inflater.inflate(R.layout.fragment_entry_list_all, container, false)
-
+        listView = view.findViewById<ListView>(R.id.entry_list)
 
         FirebaseFirestore.getInstance()
             .collection(getString(R.string.firestore_collection_path))
@@ -33,20 +32,24 @@ class EntryListFragment : Fragment() {
             .addOnSuccessListener { result ->
                 for (document in result) {
                     val entry = document.toObject(Entry::class.java)
-                    entries.add(entry)
+                    entries.add(entry.heading)
+                    Log.d("Eintrag", entry.toString())
                 }
-            }
-        val mListView = view.findViewById<ListView>(R.id.entry_list)
-        val context: Context? = activity
-        val arrayAdapter = context?.let {
-            ArrayAdapter(
-                it,
-                R.layout.entry_list_item, entries
-            )
-        }
 
-        mListView.adapter = arrayAdapter
+            }
+
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapter = ArrayAdapter(
+            requireContext(),
+            R.layout.entry_list_item,
+            entries
+        )
+        listView?.adapter = adapter
     }
 }
 
