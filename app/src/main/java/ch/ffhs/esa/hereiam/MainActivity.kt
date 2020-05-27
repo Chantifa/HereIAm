148dd97774e8510d1e4bc23e3c8955c83265f37c
Oliver Bucher <level8broccoli@gmail.com>
2020-05-27 21:24:05 +0200

    remove unused code

--- app/src/main/java/ch/ffhs/esa/hereiam/MainActivity.kt
+++ app/src/main/java/ch/ffhs/esa/hereiam/MainActivity.kt
@@ -2,7 +2,6 @@ package ch.ffhs.esa.hereiam
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -17,19 +16,10 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
 
+        // Set up bottom navigation
         val navController = findNavController(R.id.fragment_container)
         findViewById<BottomNavigationView>(R.id.bottom_navigation_menu).setupWithNavController(
             navController
         )
     }
-
-    /**
-     * Helper class to exchange fragments in the fragment container
-     */
-    fun replaceFragment(fragment: Fragment) {
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragment_container, fragment)
-        fragmentTransaction.commit()
-    }
-
 }
