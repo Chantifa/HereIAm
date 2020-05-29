b234b3c21220ef984893a6436bc93e11f6977f0d
Oliver Bucher <level8broccoli@gmail.com>
2020-05-29 22:42:06 +0200

    login form layout

--- app/src/main/java/ch/ffhs/esa/hereiam/screens/login/form/LoginFormFragment.kt
+++ app/src/main/java/ch/ffhs/esa/hereiam/screens/login/form/LoginFormFragment.kt
@@ -1,21 +1,26 @@
-package ch.ffhs.esa.hereiam.screens.login
+package ch.ffhs.esa.hereiam.screens.login.form
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import ch.ffhs.esa.hereiam.R
-import ch.ffhs.esa.hereiam.databinding.FragmentLoginBinding
+import ch.ffhs.esa.hereiam.databinding.FragmentLoginFormBinding
 
-class LoginFragment : Fragment() {
+class LoginFormFragment : Fragment() {
+
+    private val formViewModel: LoginFormViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentLoginBinding.inflate(inflater)
+        val binding = FragmentLoginFormBinding.inflate(inflater)
+
+
         binding.btnLogin.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_nav_profile_to_profileFragment))
 
         return binding.root
---
+++ app/src/main/java/ch/ffhs/esa/hereiam/screens/login/form/LoginFormViewModel.kt
@@ -0,0 +1,5 @@
+package ch.ffhs.esa.hereiam.screens.login.form
+
+import androidx.lifecycle.ViewModel
+
+class LoginFormViewModel : ViewModel()
