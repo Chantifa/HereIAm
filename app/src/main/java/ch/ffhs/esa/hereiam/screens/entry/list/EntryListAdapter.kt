ca9aeda8b3a55173e2cb6bff215774cae9e78207
Oliver Bucher <level8broccoli@gmail.com>
2020-05-29 17:24:30 +0200

    use constraint layout in list

--- app/src/main/java/ch/ffhs/esa/hereiam/screens/entry/list/EntryListAdapter.kt
+++ app/src/main/java/ch/ffhs/esa/hereiam/screens/entry/list/EntryListAdapter.kt
@@ -1,12 +1,13 @@
 package ch.ffhs.esa.hereiam.screens.entry.list
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import ch.ffhs.esa.hereiam.R
 
-class EntryListAdapter : RecyclerView.Adapter<EntryItemViewHolder>() {
+class EntryListAdapter : RecyclerView.Adapter<EntryListAdapter.ViewHolder>() {
     var data = listOf<String>()
         set(value) {
             field = value
@@ -15,14 +16,20 @@ class EntryListAdapter : RecyclerView.Adapter<EntryItemViewHolder>() {
 
     override fun getItemCount(): Int = data.size
 
-    override fun onBindViewHolder(holder: EntryItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        holder.textView.text = item
+        holder.entryTitle.text = item
+        holder.entryContent.text = item
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EntryItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.entry_list_item, parent, false) as TextView
-        return EntryItemViewHolder(view)
+        val view = layoutInflater.inflate(R.layout.entry_list_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val entryTitle: TextView =itemView.findViewById(R.id.entryTitle)
+        val entryContent: TextView =itemView.findViewById(R.id.entryContent)
     }
 }
