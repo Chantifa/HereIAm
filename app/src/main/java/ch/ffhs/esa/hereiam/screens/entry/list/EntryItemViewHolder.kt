a400d049fcc0fc36d5fc261b0ad806f802658333
Oliver Bucher <level8broccoli@gmail.com>
2020-05-29 18:09:16 +0200

    remove unused class

--- app/src/main/java/ch/ffhs/esa/hereiam/screens/entry/list/EntryItemViewHolder.kt
+++
@@ -1,6 +0,0 @@
-package ch.ffhs.esa.hereiam.screens.entry.list
-
-import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
-
-class EntryItemViewHolder (val textView: TextView): RecyclerView.ViewHolder(textView)
