504a7876e97b02ac1cea1c6be12fecb1b1e2645d
Oliver Bucher <oliver.bucher@getabstract.com>
2020-06-15 19:44:42 +0200

    inject services

--- app/src/main/java/ch/ffhs/esa/hereiam/screens/login/form/LoginFormViewModel.kt
+++ app/src/main/java/ch/ffhs/esa/hereiam/screens/login/form/LoginFormViewModel.kt
@@ -2,19 +2,20 @@ package ch.ffhs.esa.hereiam.screens.login.form
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import ch.ffhs.esa.hereiam.services.AuthenticationService
+import ch.ffhs.esa.hereiam.services.AuthenticationServiceFirebaseAuthImplementation
 import com.google.firebase.auth.FirebaseUser
 
 class LoginFormViewModel : ViewModel() {
 
     val currentUser = MutableLiveData<FirebaseUser>()
+    private val authenticationService = AuthenticationServiceFirebaseAuthImplementation()
 
     init {
-        AuthenticationService.getCurrentUser(currentUser)
+        authenticationService.getCurrentUser(currentUser)
     }
 
     fun loginUser(email: String, password: String) {
-        AuthenticationService.loginUser(email, password)
+        authenticationService.loginUser(email, password)
     }
 
 }
