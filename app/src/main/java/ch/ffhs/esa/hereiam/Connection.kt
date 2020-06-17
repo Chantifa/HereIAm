60e34b5611bc241b16819eef736c8b9ff367bd63
Oliver Bucher <oliver.bucher@getabstract.com>
2020-06-17 14:32:29 +0200

    some corrections

--- app/src/main/java/ch/ffhs/esa/hereiam/Connection.kt
+++ app/src/main/java/ch/ffhs/esa/hereiam/Connection.kt
@@ -6,16 +6,15 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.net.ConnectivityManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import ch.ffhs.esa.hereiam.services.ApiService
-import ch.ffhs.esa.hereiam.services.AuthenticationService
+import ch.ffhs.esa.hereiam.services.AuthenticationServiceFirebaseAuth
 import ch.ffhs.esa.hereiam.util.RecyclerAdapter
 import ch.ffhs.esa.hereiam.util.logout
 import kotlinx.android.synthetic.main.activity_connection.*
@@ -30,117 +29,119 @@ import timber.log.Timber
 
 class Connection : AppCompatActivity() {
 
-        private val arrayList = ArrayList<String>()
-        private var adapter = RecyclerAdapter()
-        private val retrofit = Retrofit.Builder()
-            .baseUrl("https://api.reddit.com/")
-            .addConverterFactory(ScalarsConverterFactory.create())
-            .client(OkHttpClient.Builder().build())
-            .build()
+    private val authenticationService = AuthenticationServiceFirebaseAuth()
+    private val arrayList = ArrayList<String>()
+    private var adapter = RecyclerAdapter()
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://api.reddit.com/")
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .client(OkHttpClient.Builder().build())
+        .build()
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            if (BuildConfig.DEBUG) {
-                Timber.plant(Timber.DebugTree())}
-            setContentView(R.layout.activity_connection)
-            setupRecyclerView()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
         }
+        setContentView(R.layout.activity_connection)
+        setupRecyclerView()
+    }
 
-        override fun onStart() {
-            super.onStart()
-            registerReceiver(
-                broadcastReceiver,
-                IntentFilter(ACCESS_NETWORK_STATE)
-            )
+    override fun onStart() {
+        super.onStart()
+        registerReceiver(
+            broadcastReceiver,
+            IntentFilter(ACCESS_NETWORK_STATE)
+        )
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(broadcastReceiver)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.option_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        item?.let {
+            super.onOptionsItemSelected(item)
         }
+        if (item?.itemId == R.id.action_logout) {
 
-        override fun onStop() {
-            super.onStop()
-            unregisterReceiver(broadcastReceiver)
-        }
+            AlertDialog.Builder(this).apply {
+                setTitle(getString(R.string.logout_question))
+                setPositiveButton(getString(R.string.logout_yes)) { _, _ ->
 
-        override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-            super.onCreateOptionsMenu(menu)
-            menuInflater.inflate(R.menu.option_menu, menu)
-            return true
-        }
+                    authenticationService.signOut()
+                    this.context.logout()
 
-        override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-            item?.let {
-                super.onOptionsItemSelected(item)
-            }
-            if (item?.itemId == R.id.action_logout) {
-
-                AlertDialog.Builder(this).apply {
-                    setTitle(getString(R.string.logout_question))
-                    setPositiveButton(getString(R.string.logout_yes)) { _, _ ->
-
-                        AuthenticationService.signOut()
-                        this.context.logout()
-
-                    }
-                    setNegativeButton(getString(R.string.logout_abort)) { _, _ ->
-                    }
-                }.create().show()
-
-            }
-            return true
-        }
-
-        private var broadcastReceiver: BroadcastReceiver = object : BroadcastReceiver() {
-            override fun onReceive(context: Context, intent: Intent) {
-                val notConnected = intent.getBooleanExtra(
-                    ConnectivityManager
-                    .EXTRA_NO_CONNECTIVITY, false)
-                if (notConnected) {
-                    disconnected()
-                } else {
-                    connected()
                 }
+                setNegativeButton(getString(R.string.logout_abort)) { _, _ ->
+                }
+            }.create().show()
+
+        }
+        return true
+    }
+
+    private var broadcastReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val notConnected = intent.getBooleanExtra(
+                ConnectivityManager
+                    .EXTRA_NO_CONNECTIVITY, false
+            )
+            if (notConnected) {
+                disconnected()
+            } else {
+                connected()
             }
         }
+    }
 
-        private fun setupRecyclerView() {
-            with(recyclerView){
-                val layoutManager = LinearLayoutManager(this@Connection)
-                adapter = this@Connection.adapter
-            }
+    private fun setupRecyclerView() {
+        with(recyclerView) {
+            val layoutManager = LinearLayoutManager(this@Connection)
         }
+    }
 
-        private fun disconnected() {
-            recyclerView.visibility = View.INVISIBLE
-            noSignal.visibility = View.VISIBLE
+    private fun disconnected() {
+        recyclerView.visibility = View.INVISIBLE
+        noSignal.visibility = View.VISIBLE
+    }
+
+    private fun connected() {
+        recyclerView.visibility = View.VISIBLE
+        noSignal.visibility = View.INVISIBLE
+        fetchFeeds()
+    }
+
+    private fun fetchFeeds() {
+        retrofit.create(ApiService::class.java)
+            .getFeeds()
+            .enqueue(object : Callback<String> {
+                override fun onFailure(call: Call<String>, t: Throwable) {
+                    Timber.e(t)
+                }
+
+                override fun onResponse(call: Call<String>?, response: Response<String>) {
+                    addTitleToList(response.body()!!)
+                }
+
+            })
+    }
+
+    private fun addTitleToList(response: String) {
+        val jsonObject = JSONObject(response).getJSONObject("data")
+        val children = jsonObject.getJSONArray("children")
+
+        for (i in 0 until children.length()) {
+            val item = children.getJSONObject(i).getJSONObject("data").getString("title")
+            arrayList.add(item)
+            adapter.setItems(arrayList)
         }
-
-        private fun connected() {
-            recyclerView.visibility = View.VISIBLE
-            noSignal.visibility = View.INVISIBLE
-            fetchFeeds()
-        }
-
-        private fun fetchFeeds() {
-            retrofit.create(ApiService::class.java)
-                .getFeeds()
-                .enqueue(object : Callback<String> {
-                    override fun onFailure(call: Call<String>, t: Throwable) {
-                        Log.e("MainActivityTag", t.message)
-                    }
-
-                    override fun onResponse(call: Call<String>?, response: Response<String>) {
-                        addTitleToList(response.body()!!)
-                    }
-
-                })
-        }
-
-        private fun addTitleToList(response: String) {
-            val jsonObject = JSONObject(response).getJSONObject("data")
-            val children = jsonObject.getJSONArray("children")
-
-            for (i in 0..(children.length()-1)) {
-                val item = children.getJSONObject(i).getJSONObject("data").getString("title")
-                arrayList.add(item)
-                adapter.setItems(arrayList)
-            }
-        }
+    }
 }
