693f8ea1ee7a7369844d182b1747f3fa21b1eb3f
Oliver Bucher <oliver.bucher@getabstract.com>
2020-06-17 14:23:24 +0200

    clean up StorageService.kt

--- app/src/main/java/ch/ffhs/esa/hereiam/screens/profile/ProfileViewModel.kt
+++ app/src/main/java/ch/ffhs/esa/hereiam/screens/profile/ProfileViewModel.kt
@@ -2,64 +2,28 @@ package ch.ffhs.esa.hereiam.screens.profile
 
 import android.graphics.Bitmap
 import android.net.Uri
-import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import ch.ffhs.esa.hereiam.databinding.FragmentProfileBinding
 import ch.ffhs.esa.hereiam.services.AuthenticationServiceFirebaseAuth
 import ch.ffhs.esa.hereiam.services.StorageServiceFirebase
-import ch.ffhs.esa.hereiam.util.toast
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.storage.StorageReference
-import com.google.firebase.storage.UploadTask
-import java.io.ByteArrayOutputStream
 
 class ProfileViewModel : ViewModel() {
 
     val currentUser = MutableLiveData<FirebaseUser>()
+    private val userPicture = MutableLiveData<Uri>()
     private val authenticationService = AuthenticationServiceFirebaseAuth()
     private val storageService = StorageServiceFirebase()
+    private val folder = "profileImages"
 
     init {
-        authenticationService.getCurrentUser().observeForever { result -> currentUser.value = result }
+        authenticationService.getCurrentUser()
+            .observeForever { result -> currentUser.value = result }
     }
 
-    private lateinit var imageUri: Uri
-    private lateinit var storageRef: StorageReference
-
-    fun uploadImage(
-        bitmap: Bitmap,
-        baos: ByteArrayOutputStream
-    ): UploadTask {
-        val (storageRef, upload) = storageService.uploadImage(bitmap, baos, currentUser.value)
-        this.storageRef = storageRef
-        return upload
-    }
-
-    fun waitOnUpload(
-        upload: UploadTask,
-        bitmap: Bitmap,
-        binding: FragmentProfileBinding,
-        context: FragmentActivity?
-    ) {
-        upload.addOnCompleteListener { uploadTask ->
-            try {
-                if (uploadTask.isSuccessful) {
-                    storageRef.downloadUrl.addOnCompleteListener { urlTask ->
-                        urlTask.result?.let {
-                            imageUri = it
-                            context?.toast(imageUri.toString())
-                            binding.profileAvatar.setImageBitmap(bitmap)
-                        }
-                    }
-                } else {
-                    uploadTask.exception?.let {
-                        context?.toast(it.message!!)
-                    }
-                }
-            } catch (e: Exception) {
-                context?.toast(e.message!!)
-            }
-        }
+    fun uploadImage(bitmap: Bitmap) {
+        val userId = currentUser.value?.uid
+        storageService.uploadImage(bitmap, folder, userId ?: "")
+            .observeForever { result -> userPicture.value = result }
     }
 }
--- app/src/main/java/ch/ffhs/esa/hereiam/services/StorageService.kt
+++ app/src/main/java/ch/ffhs/esa/hereiam/services/StorageService.kt
@@ -1,18 +1,27 @@
 package ch.ffhs.esa.hereiam.services
 
 import android.graphics.Bitmap
-import com.google.firebase.auth.FirebaseUser
+import android.net.Uri
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.StorageReference
-import com.google.firebase.storage.UploadTask
 import java.io.ByteArrayOutputStream
+import kotlin.random.Random
 
 interface StorageService {
     fun uploadImage(
         bitmap: Bitmap,
-        baos: ByteArrayOutputStream,
-        currentUser: FirebaseUser?
-    ): Pair<StorageReference, UploadTask>
+        folder: String,
+        fileName: String = getRandomString(20)
+    ): LiveData<Uri>
+
+    private fun getRandomString(length: Int): String {
+        val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+        return (1..length)
+            .map { Random.nextInt(0, charPool.size) }
+            .map(charPool::get)
+            .joinToString("")
+    }
 }
 
 class StorageServiceFirebase : StorageService {
@@ -20,16 +29,31 @@ class StorageServiceFirebase : StorageService {
 
     override fun uploadImage(
         bitmap: Bitmap,
-        baos: ByteArrayOutputStream,
-        currentUser: FirebaseUser?
-    ): Pair<StorageReference, UploadTask> {
-        val storageRef = fbStorage
-            .reference
-            .child("pics/${currentUser?.uid}")
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
-        val image = baos.toByteArray()
+        folder: String,
+        fileName: String
+    ): MutableLiveData<Uri> {
+        val storageReference = fbStorage.reference.child("$folder/$fileName.jpg")
+        val outputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+        val byteArray = outputStream.toByteArray()
+        val uploadTask = storageReference.putBytes(byteArray)
 
-        val upload = storageRef.putBytes(image)
-        return Pair(storageRef, upload)
+        val url = MutableLiveData<Uri>()
+        uploadTask.continueWithTask { task ->
+            if (!task.isSuccessful) {
+                task.exception?.let {
+                    throw it
+                }
+            }
+            storageReference.downloadUrl
+        }.addOnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                task.exception?.let {
+                    throw it
+                }
+            }
+            url.value = task.result
+        }
+        return url
     }
 }
