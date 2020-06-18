fb741e96988b6868d0f8d86ccc2e79ec60114ade
Oliver Bucher <oliver.bucher@getabstract.com>
2020-06-18 15:14:48 +0200

    coroutines improvement

--- app/src/main/java/ch/ffhs/esa/hereiam/screens/login/form/LoginFormViewModel.kt
+++ app/src/main/java/ch/ffhs/esa/hereiam/screens/login/form/LoginFormViewModel.kt
@@ -1,9 +1,11 @@
 package ch.ffhs.esa.hereiam.screens.login.form
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.liveData
 import ch.ffhs.esa.hereiam.services.AuthenticationService
 import ch.ffhs.esa.hereiam.services.AuthenticationServiceFirebaseAuth
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
 import timber.log.Timber
 
 class LoginFormViewModel : ViewModel() {
@@ -11,11 +13,13 @@ class LoginFormViewModel : ViewModel() {
     private val authenticationService: AuthenticationService = AuthenticationServiceFirebaseAuth()
 
     fun loginUser(email: String, password: String) {
-        val result = liveData {
-            emit(authenticationService.loginUser(email, password))
-        }
-        result.observeForever {
-            Timber.e("Login finished: $it")
+        CoroutineScope(IO).launch {
+            val result = authenticationService.loginUser(email, password)
+            if (result == null) {
+                Timber.e("Login error")
+            } else {
+                Timber.e("Login successfully: $result")
+            }
         }
     }
 }
