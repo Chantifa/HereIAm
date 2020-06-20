8c9ef7c818cce17ad9a93918c49561124695aab3
chantale.gihara <chantale.gihara@studnets.ffhs.ch>
2020-06-20 10:47:38 +0200

    ContextExtension.kt

---
+++ app/src/main/java/ch/ffhs/esa/hereiam/util/ContextExtension.kt
@@ -0,0 +1,7 @@
+package ch.ffhs.esa.hereiam.util
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.toast(message: String) =
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
