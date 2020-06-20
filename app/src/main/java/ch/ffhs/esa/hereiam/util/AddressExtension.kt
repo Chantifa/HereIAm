05a42604e6b8bd7f325c062d9d75800c54b5b4f6
Oliver Bucher <oliver.bucher@getabstract.com>
2020-06-20 11:32:42 +0200

    remove debug log

--- app/src/main/java/ch/ffhs/esa/hereiam/util/AddressExtension.kt
+++ app/src/main/java/ch/ffhs/esa/hereiam/util/AddressExtension.kt
@@ -1,10 +1,8 @@
 package ch.ffhs.esa.hereiam.util
 
 import android.location.Address
-import timber.log.Timber
 
 fun Address?.getShortAddress(): String? {
-    Timber.e("$this")
     if (this == null) return null
 
     var string = ""
