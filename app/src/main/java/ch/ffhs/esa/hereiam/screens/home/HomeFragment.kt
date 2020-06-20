57b67228756d41176a21ad0917da4df3161a0b68
Oliver Bucher <oliver.bucher@getabstract.com>
2020-06-20 10:33:48 +0200

    remove duplicated location code

--- app/src/main/java/ch/ffhs/esa/hereiam/screens/home/HomeFragment.kt
+++ app/src/main/java/ch/ffhs/esa/hereiam/screens/home/HomeFragment.kt
@@ -1,28 +1,21 @@
 package ch.ffhs.esa.hereiam.screens.home
 
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import ch.ffhs.esa.hereiam.R
 import ch.ffhs.esa.hereiam.databinding.FragmentHomeBinding
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
-
-const val PERMISSION_ID = 42
 
 class HomeFragment : Fragment(), OnMapReadyCallback {
     private val viewModel: HomeViewModel by viewModels()
     lateinit var binding: FragmentHomeBinding
-    private lateinit var mFusedLocationClient: FusedLocationProviderClient
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
@@ -57,36 +50,14 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
         }
         viewModel.activity = requireActivity()
         viewModel.onActivity()
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
-        viewModel.mFusedLocationClient = mFusedLocationClient
         return binding.root
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        when (requestCode) {
-            PERMISSION_ID -> {
-                // If request is cancelled, the result arrays are empty.
-                if ((grantResults.isNotEmpty() &&
-                            grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                ) {
-                    viewModel.getLastLocation()
-                } else {
-                    Toast.makeText(activity, "Aktiviere Standort!", Toast.LENGTH_SHORT).show()
-                }
-                return
-            }
-
-        }
-    }
 
     override fun onMapReady(map: GoogleMap?) {
         map?.let {
             viewModel.googleMap = it
-            viewModel.getLastLocation()
+            viewModel.updateMarkerOnMap()
         }
     }
 }
--- app/src/main/java/ch/ffhs/esa/hereiam/screens/home/HomeViewModel.kt
+++ app/src/main/java/ch/ffhs/esa/hereiam/screens/home/HomeViewModel.kt
@@ -1,31 +1,18 @@
 package ch.ffhs.esa.hereiam.screens.home
 
-import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.pm.PackageManager
 import android.location.Geocoder
-import android.location.LocationManager
-import android.os.Looper
-import android.widget.Toast
-import android.widget.Toast.makeText
-import androidx.core.app.ActivityCompat
-import androidx.core.app.ActivityCompat.requestPermissions
 import androidx.core.app.ComponentActivity
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import ch.ffhs.esa.hereiam.HereIAmApplication
-import ch.ffhs.esa.hereiam.R
 import ch.ffhs.esa.hereiam.services.LocationService
 import ch.ffhs.esa.hereiam.services.LocationServiceImplementation
 import ch.ffhs.esa.hereiam.util.getShortAddress
 import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationResult
-import com.google.android.gms.location.LocationServices.getFusedLocationProviderClient
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.LatLng
@@ -85,16 +72,6 @@ class HomeViewModel : ViewModel() {
         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 10f))
     }
 
-    private fun loadDefaultLocation() {
-
-        val latitude = 46.948162
-        val longitude = 7.436944
-        // Location of Bern
-        _locationName.value = "FFHS Bern Welle 7"
-
-        setLocationOnGoogleMaps(LatLng(latitude, longitude))
-    }
-
     fun changeMapBasedOnUserInput(
         query: String
     ) {
@@ -107,86 +84,10 @@ class HomeViewModel : ViewModel() {
         }
     }
 
-    @SuppressLint("MissingPermission")
-    fun getLastLocation() {
-        if (checkPermissions()) {
-            if (isLocationEnabled()) {
-                mFusedLocationClient.lastLocation.addOnCompleteListener(activity) { task ->
-                    val location: android.location.Location? = task.result
-                    if (location == null) {
-                        requestNewLocationData()
-                    } else {
-                        val latitude = location.latitude
-                        val longitude = location.longitude
-                        val latLng = LatLng(latitude, longitude)
-                        _location.postValue(latLng)
-                        currentLocation.postValue(latLng)
-                        setLocationOnGoogleMaps(latLng)
-                    }
-                }
-            } else {
-                val toast =
-                    makeText(context, context?.getString(R.string.please_activate_access_on_location), Toast.LENGTH_SHORT)
-                toast.show()
-                loadDefaultLocation()
-            }
-        } else {
-            requestPermissions(
-                activity,
-                arrayOf(
-                    Manifest.permission.ACCESS_COARSE_LOCATION,
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                ),
-                PERMISSION_ID
-            )
-        }
-    }
-
-    private fun isLocationEnabled(): Boolean {
-        val locationManager = activity.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
-            LocationManager.NETWORK_PROVIDER
-        )
-    }
-
-    private fun checkPermissions(): Boolean {
-        if (ActivityCompat.checkSelfPermission(
-                activity,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED &&
-            ActivityCompat.checkSelfPermission(
-                activity,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            return true
-        }
-        return false
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun requestNewLocationData() {
-        val mLocationRequest = LocationRequest()
-        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        mLocationRequest.interval = 0
-        mLocationRequest.fastestInterval = 0
-        mLocationRequest.numUpdates = 1
-        mFusedLocationClient = getFusedLocationProviderClient(activity)
-        mFusedLocationClient.requestLocationUpdates(
-            mLocationRequest, mLocationCallback,
-            Looper.myLooper()
-        )
-    }
-
-    private val mLocationCallback = object : LocationCallback() {
-        override fun onLocationResult(locationResult: LocationResult) {
-            val mLastLocation: android.location.Location = locationResult.lastLocation
-            val latitude = mLastLocation.latitude
-            val longitude = mLastLocation.longitude
-            val latLng = LatLng(latitude, longitude)
-            _location.postValue(latLng)
-            currentLocation.postValue(latLng)
-            setLocationOnGoogleMaps(latLng)
+    fun updateMarkerOnMap() {
+        val currentLocation = HereIAmApplication.currentLocation
+        currentLocation?.let { adr ->
+            setLocationOnGoogleMaps(LatLng(adr.latitude, adr.longitude))
         }
     }
 }
